613e7cf905783aa3de6d2844f803264e8ae5e496
kishibee <raflyade9@gmail.com>
2023-12-03 08:52:58 -0800

    starter

--- AndroidStudioProjects/CattleDiag/app/src/main/java/com/example/cattlediag/presentation/MainActivity.kt
+++ AndroidStudioProjects/CattleDiag/app/src/main/java/com/example/cattlediag/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.cattlediag
+package com.example.cattlediag.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cattlediag.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
